db5ed40b47647df316e9e6734d49a6e85fe85f1d
Mark Sherriff <marksherriff@github.com>
2015-10-11 23:22:56 -0400

    Initial Commit

---
+++ 4720Example0/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  4720Example0
+//
+//  Created by sherriff on 10/11/15.
+//  Copyright © 2015 Mark Sherriff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // MARK: Properties
+    @IBOutlet weak var labelView: UILabel!
+
+    @IBOutlet weak var textField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: Actions
+
+    @IBAction func setLabelText(sender: UIButton) {
+        labelView.text = textField.text
+    }
+   
+
+}
+
